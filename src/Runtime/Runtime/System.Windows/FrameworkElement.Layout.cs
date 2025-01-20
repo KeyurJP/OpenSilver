44a6f68666c90bb6e192c869a579ce18cb1bf1fe
Pierre Barjon <barjon.pierre@gmail.com>
2025-01-20 17:06:23 +0100

    fix: prevent Selector from replacing "IsSelected" bindings on its containers

--- src/Runtime/Runtime/System.Windows/FrameworkElement.Layout.cs
+++ src/Runtime/Runtime/System.Windows/FrameworkElement.Layout.cs
@@ -434,7 +434,6 @@ namespace System.Windows
         /// <summary>
         /// Occurs when the layout of the Silverlight visual tree changes.
         /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
         public new event EventHandler LayoutUpdated
         {
             add => base.LayoutUpdated += value;
